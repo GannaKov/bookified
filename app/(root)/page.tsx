8f33a7f8ba506a47b3e56413aec172f8b9c87d55
Ganna Kovchyk <zlatta2000@gmail.com>
2026-06-19 17:29:01 +0200

    init commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import HeroSection from '@/components/HeroSection'
+import {sampleBooks} from "@/lib/lib/constants";
+import BookCard from "@/components/BookCard";
+
+const Page = () => {
+    return (
+        <main className="wrapper container">
+            <HeroSection />
+            <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-5 mb-10">
+                <h2 className="text-3xl font-serif font-bold text-[#212a3b]">Recent Books</h2>
+                {/*<Search />*/}
+
+            </div>
+
+            <div className="library-books-grid">
+                {sampleBooks.map((book) => (
+                    <BookCard key={book._id} title={book.title} author={book.author} coverURL={book.coverURL} slug={book.slug} />
+                ))}
+            </div>
+        </main>
+    )
+}
+export default Page
+
+
+
